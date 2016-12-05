68d0202d3549b0a032400a2aed095a3201a12b55
doronrot <doronrot17@gmail.com>
2016-12-06 00:03:51 +0200

    with set,&applic, mit still doesn't work

--- compiler.scm
+++ compiler.scm
@@ -9,14 +9,17 @@
 			(char? exp)
 			(string? exp) )))
 
+(define not-reserved?
+	(lambda (x)
+		(not(member x '(and begin cond define do else if lambda 
+					  	     let let* letrec or quasiquote unquote
+					 	     unquote-splicing quote set!)))))
 
+;TODO: still have a problem when number appears at the beginning
 (define var?
 	(lambda (x)
 		(and (symbol? x)
-			 (not (member x '(and begin cond define do else if lambda 
-					  	     let let* letrec or quasiquote unquote
-					 	     unquote-splicing quote set!))))))
-;still have a problem when number appear at the beginning
+			 (not-reserved? x))))
 
 (define identify-lambda
 	(lambda (argl ret-simple ret-opt ret-var)
@@ -27,10 +30,11 @@
 					(lambda (s) (ret-simple `(,(car argl) ,@s))) ;simple
 					(lambda (s opt) (ret-opt `(,(car argl) ,@s) opt)) ;opt
 					(lambda (var) (ret-opt `(,(car argl)) var)))))))
-					
                                 
 (define parse
 	(let ((run (compose-patterns
+
+					;Constants;
 					(pattern-rule
 						`(quote ,(? 'c))
 						(lambda (c) `(const ,c)))
@@ -38,8 +42,7 @@
 						(? 'c simple-const?)
 						(lambda (c) `(const ,c)))
 
-
-
+					;Conditionals;
 					(pattern-rule
 						`(if ,(? 'test) ,(? 'dit))
 						(lambda (test dit) 
@@ -48,6 +51,8 @@
 						`(if ,(? 'test) ,(? 'dit) ,(? 'dif))
 						(lambda (test dit dif)
 							`(if3 ,(parse test) ,(parse dit) ,(parse dif))))
+					
+					;Disjunctions;
 					(pattern-rule
 						`(or)
 						(lambda () #f))
@@ -57,47 +62,59 @@
 							`(or ,(map parse
 								 	   (cons expr exprs)))))
                                         
-                                        ;TODO: maybe should be begin/seq?
+                    ;Lambda forms;       
 					(pattern-rule
 						`(lambda ,(? 'argl) ,(? 'expr).,(? 'exprs))
 						(lambda (argl expr exprs)
-					
-                                                    (identify-lambda    argl 
-                                                                        (lambda (s) `(lambda-simple ,s ,(parse `(begin ,@(cons expr exprs)))))
-                                                                        (lambda (s opt) `(lambda-opt ,s ,opt ,(parse `(begin ,@(cons expr exprs)))))
-                                                                        (lambda (var) `(lambda-var ,var ,(parse `(begin ,@(cons expr exprs))))))))
-                                        
-		    			;define
+							(identify-lambda    argl 
+                                                (lambda (s) `(lambda-simple ,s ,(parse `(begin ,@(cons expr exprs)))))
+                                                (lambda (s opt) `(lambda-opt ,s ,opt ,(parse `(begin ,@(cons expr exprs)))))
+                                                (lambda (var) `(lambda-var ,var ,(parse `(begin ,@(cons expr exprs))))))))
+                
+		    		;Define;
 					(pattern-rule	
 						`(define ,(? 'var var?) ,(? 'expr))
 						(lambda (var expr)
 							`(def (var ,var) ,(parse expr))))
-					;defineMitFunc
-					(pattern-rule
-						`(define (,(? 'var var?) ,(? 'argsl)) ,(? 'expr) . ,(? 'exprs))
-						(lambda (var argl expr exprs)
- 							`(def (var ,var) ,(parse `(lambda ,argl ,expr ,exprs)))))
+					
+					; ;defineMitFunc
+					; (pattern-rule
+					; 	`(define (,(? 'var var?) ,(? 'args) ,(? 'args2)) ,(? 'expr) . ,(? 'exprs))
+					; 	(lambda (var args  args2 expr exprs)
+					; 		(display args2)
+					; 		(parse `(define ,var (lambda ,(cons args args2) ,expr ,@exprs)))))
 
-		    
+
+ 					;Assignments;
+				    (pattern-rule
+					 	`( set! ,(? 'v) ,(? 'pexpr))
+					 	(lambda (v pexpr)
+							`(set (var ,v) ,(parse pexpr)))) 
+
+				    ;Applications;
+				    (pattern-rule
+					 	`( ,(? 'expr not-reserved?) .,(? 'exprs))
+					 	(lambda (expr exprs)
+
+							`(applic ,(parse expr) ,(map parse exprs))))
+		    		
+		    		;Sequences;
 					(pattern-rule
 						`(begin)
 						 (lambda () `(const ,(void))))
 					(pattern-rule
 						`(begin ,(? 'expr).,(? 'exprs))
 						(lambda (expr exprs)
-                                                    (if (null? exprs)
-                                                        (parse expr)
-							`(seq ,(map parse (cons expr exprs) )))))
-
-                                        ;TODO: forum question about "var?"
+                            (if (null? exprs)
+                                (parse expr)
+							   `(seq ,(map parse (cons expr exprs) )))))
+                     
+					;Variables;
+					;TODO: forum question about "var?"
 					(pattern-rule
 					 	(? 'v var?)				
-					 	(lambda (v) `(var ,v)))	
-					)))
+					 	(lambda (v) `(var ,v))))	
+					))
+				
 		(lambda (sexpr)
 			(run sexpr (lambda () 'fail!)))))
-
-
-;  (load "compiler.scm")
-
- ; (parse '(lambda args a b c))
