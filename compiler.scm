c7a3e019c0d634abf9a3e3d27e231f18ca9f8663
mayansal <mayan32121@gmail.com>
2016-12-05 21:01:23 +0200

    Update compiler.scm

--- compiler.scm
+++ compiler.scm
@@ -1,5 +1,5 @@
 (load "pattern-matcher.scm")
-
+helloooooooooooooooo
 (define simple-const? 
 	(lambda (exp)
 		(or (number? exp)
@@ -8,7 +8,7 @@
 			(vector? exp)
 			(char? exp)
 			(string? exp) )))
-asdg
+
 
 (define var?
 	(lambda (x)
