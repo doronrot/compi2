369aaa9e8bfb97d8b0edb77e505ff5748929fbf8
doronrot <doronrot17@gmail.com>
2016-12-16 04:16:54 +0200

    Update cse.scm

--- cse.scm
+++ cse.scm
@@ -3,22 +3,38 @@
 
 (define cse
 	(lambda (expr)
-		(cse_core expr)))
+		(let* ((double_answer (cse_core expr (list))))
+			 ;  (new_list (car double_answer))
+			  ; (vars_list (cdr double_answer)))
+			double_answer)))
 
 
+
+ ; (define cse_core
+ ; 	(lambda (expr)
+	; 	(if (or (not (list? expr)) (null? expr)) 
+	; 		expr
+ ; 			(let ((first_applic (find_first_applic expr)))
+ ; 				(if (occur_again? first_applic expr)
+ ; 					(let* ((new_name (gensym))
+ ; 					  	  (new_list (replace_occurances first_applic expr new_name)))
+ ; 						(cse_core new_list ))
+ ; 					(append (list (car expr))
+ ; 						    (cse_core (cdr expr))))))))
 
  (define cse_core
- 	(lambda (expr)
+ 	(lambda (expr var_list)
 		(if (or (not (list? expr)) (null? expr)) 
-			expr
+			(list expr var_list)
  			(let ((first_applic (find_first_applic expr)))
  				(if (occur_again? first_applic expr)
  					(let* ((new_name (gensym))
- 					  	  (new_list (replace_occurances first_applic expr new_name)))
- 						(cse_core new_list))
+ 					  	   (new_list (replace_occurances first_applic expr new_name))
+ 					  	   (updated_var_list (append var_list
+ 					  	  							(list (cons new_name (list first_applic))))))
+ 						(cse_core new_list updated_var_list))
  					(append (list (car expr))
- 						    (cse_core (cdr expr))))))))
-
+ 						    (cse_core (cdr expr) var_list)))))))
 
 
 (define replace_occurances
@@ -70,3 +86,26 @@
 					 		 (null? (cdr first))))
 					(find_first_applic_help rest (append acc (list first)))
 					(find_first_applic_help first (list)))))))
+
+;version 2 (letrec): (doesnt work)
+; (define find_first_applic
+; 	(lambda (lst)
+; 		(letrec ((loop 
+; 					(lambda (orig_lst acc)
+; 						(if (null? orig_lst)
+; 							acc
+; 							(let ((first (car orig_lst))
+; 								  (rest (cdr orig_lst)))
+; 								(if (or (atom? first) 
+; 									    (null? first)
+; 									    (equal? first `'())
+; 									    (and (equal? (car first) 'list)
+; 									 		 (null? (cdr first))))
+; 									(find_first_applic rest (append acc (list first)))
+; 									(find_first_applic first (list))))))))
+; 			(loop lst (list)))))
+
+
+
+
+
